7d5f545103db3cdde7929c3db4a4afe1cf48a286
Muhammed-Rasalcc <rasalcc9@gmail.com>
2025-05-02 12:11:54 +0530

    resume link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -46,7 +46,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1ViYbYhO8DBzTfFGMFoYcSweMHPWAiT4J/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1x2y4wpx9n0PiEH1DUroahjguat1UNzVH/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
