ec6e812f33542b77cf5106de461009b03a922652
Muhammed-Rasalcc <rasalcc9@gmail.com>
2025-05-02 12:40:02 +0530

    image

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactTypingEffect from 'react-typing-effect'
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/porfile.png';
+import profileImage from '../../assets/porfile.jpg';
 
 const About = () => {
   return (
